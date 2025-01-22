6bb96879924625d11d45ed9805bdb2818fac4d02
César Godinho <cesarsky5@gmail.com>
2025-01-22 10:05:03 +0000

    Add NIDAQmx module boilerplate.

---
+++ cmake/modules/FindLibNIDAQmx.cmake
@@ -0,0 +1,25 @@
+# For native x86_64
+set(NI_PPATH "National Instruments/Shared/ExternalCompilerSupport/C")
+set(_pf_x86 "$ENV{ProgramFiles\(x86\)}/${NI_PPATH}")
+list(APPEND NIDAQMX_PATH ${_pf_x86})
+
+# Search on any other locations case necessary
+list(APPEND NIDAQMX_PATH "H:/NI/Shared/ExternalCompilerSupport/C")
+
+# Find installed library using CMake functions
+find_library(LIBNIDAQMX_LIBRARY 
+			NAMES "NIDAQmx"
+            PATHS ${NIDAQMX_PATH}
+            PATH_SUFFIXES "lib64/msvc;lib64/gcc")
+            
+find_path(LIBNIDAQMX_INCLUDE_DIR 
+			NAMES "NIDAQmx.h"
+            PATHS ${NIDAQMX_PATH}
+            PATH_SUFFIXES "include")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibNIDAQmx  DEFAULT_MSG
+                                  LIBNIDAQMX_LIBRARY LIBNIDAQMX_INCLUDE_DIR)
+
+# Dont display these vars to the user
+mark_as_advanced(LIBNIDAQMX_LIBRARY LIBNIDAQMX_INCLUDE_DIR)
---
+++ nidaq/main.cpp
@@ -0,0 +1,9 @@
+#include <NIDAQmx.h>
+#include <cstdint>
+#include <optional>
+#include <string>
+
+static std::optional<std::string> NIGetError(std::int32_t error)
+{
+	return {};
+}
