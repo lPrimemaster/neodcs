1df1d0aa7b00aea84900a5cbfb5c1e4613e69052
lPrimemaster <cesarsky5@gmail.com>
2025-03-19 12:29:43 +0000

    Change code to use an older version of the AXAS-D api for older devices.

--- axas_mca/main.cpp
+++ axas_mca/main.cpp
@@ -2,8 +2,13 @@
 // Author : César Godinho
 //   Date : 18/03/2025
 
+#include <filesystem>
+#define _CRT_SECURE_NO_WARNINGS
 #include <WinSock2.h>
-#include <vicolib.h>
+#include <handel.h>
+#include <handel_errors.h>
+#include <handel_generic.h>
+#include <md_generic.h>
 #include <mxbackend.h>
 
 class AxasMCA : public mulex::MxBackend
@@ -14,187 +19,211 @@ public:
 
 private:
 	std::int32_t devindex;
-	InterfaceType devinterface;
-	CharType serial[12];
-	CharType ipaddr[12];
-	std::vector<UInt8Type> mcadata;
 	bool status;
-	UInt16Type bins;
-	UInt8Type bpb;
+	unsigned int devchannels;
+	std::vector<std::uint8_t> mcadata;
 
 protected:
 	void startMeasurement(std::uint64_t runno);
 	void stopMeasurement(std::uint64_t runno);
 
 private:
-	void setupBinsAndTimings();
-	void updateStats();
+	unsigned int findChannels();
+	void update();
 
 	template<typename F, typename... Args>
 	void runWithCheck(F f, Args... args)
 	{
 		const int status = f(args...);
-		if(status != DPP_SUCCESS && status != VICO_SUCCESS && status != MCU_SUCCESS)
+		if(status != XIA_SUCCESS)
 		{
-			log.error("Error! Status = %#02x.", status);
+			log.error("Error! Status = %#02x. Message = %s.", status, xiaGetErrorText(status));
+			// xiaExit();
+			// xiaCloseLog();
 		}
+	}
+
+	template<typename T>
+	void setupParameter(std::string name, T defvalue)
+	{
+		std::string pkey = "/user/axas/" + name;
+		if constexpr(std::is_same_v<T, double>) rdb[pkey].create(mulex::RdbValueType::FLOAT64, defvalue);
+		else static_assert(false, "Type not allowed on setup parameter.");
+
+		T value = rdb[pkey];
+		log.info("Setting up %s: %s.", name.c_str(), std::to_string(defvalue).c_str());
+		runWithCheck(xiaSetAcquisitionValues, 0, name.data(), (void*)&value);
 	}
 };
 
 AxasMCA::AxasMCA(int argc, char* argv[]) : mulex::MxBackend(argc, argv), status(false)
 {
-	// Init usb device
-	runWithCheck(scanUsbDevices);
+	// Setup logging to file
+	xiaSetLogLevel(MD_DEBUG);
+	rdb["/user/axas/logpath"].create(mulex::RdbValueType::STRING, mulex::mxstring<512>("handel.log"));
+	mulex::mxstring<512> logpath = rdb["/user/axas/logpath"];
+	xiaSetLogOutput(const_cast<char*>(logpath.c_str()));
 
-	UInt8Type ndevices = 0;
-	runWithCheck(getNumberOfDevices, &ndevices);
+	// Startup system
+	rdb["/user/axas/inipath"].create(mulex::RdbValueType::STRING, mulex::mxstring<512>("DPP2.ini"));
+	mulex::mxstring<512> inipath = rdb["/user/axas/inipath"];
 
-	if(ndevices == 0)
+	if(!std::filesystem::is_regular_file(inipath.c_str()))
 	{
-		log.warn("No AXAS device found. Aborting init...");
-		log.info("Check connections and restart this backend.");
+		log.error(
+			"File [%s] not found. Make sure you use a correct DPP2 ini file. "
+			"Can be copied from the default generation from ProSpect 1.1.",
+			inipath.c_str()
+		);
 		return;
 	}
 
-	log.info("Found #%d devices.", ndevices);
+	char* cinipath = const_cast<char*>(inipath.c_str());
+	runWithCheck(xiaInit, cinipath);
 
-	for(int i = 0; i < ndevices; i++)
+	// Boot hardware
+	runWithCheck(xiaStartSystem);
+
+	devchannels = findChannels();
+
+	// Setup DPP parameters
+	setupParameter("parset", 3.0);
+	setupParameter("genset", 0.0);
+	setupParameter("number_mca_channels", 4096.0);
+	setupParameter("trigger_threshold", 25.0);
+	setupParameter("gain", 5.700);
+	setupParameter("bytes_per_bin", 3.0);
+
+	// Apply parameters
+	std::string cmd = "apply";
+	unsigned short ignored = 0;
+	runWithCheck(xiaBoardOperation, 0, cmd.data(), (void*)&ignored);
+
+	// Save presets
+	cmd = "save_genset";
+	double genset = rdb["/user/axas/genset"];
+	unsigned short gensets = static_cast<unsigned short>(genset);
+	runWithCheck(xiaBoardOperation, 0, cmd.data(), (void*)&gensets);
+	cmd = "save_parset";
+	double parset = rdb["/user/axas/parset"];
+	unsigned short parsets = static_cast<unsigned short>(parset);
+	runWithCheck(xiaBoardOperation, 0, cmd.data(), (void*)&parsets);
+	
+	// Find current peaking times
+	cmd = "get_number_pt_per_fippi";
+	unsigned short npt = 0;
+	std::vector<double> peakingtimes;
+	runWithCheck(xiaBoardOperation, 0, cmd.data(), &npt);
+	peakingtimes.resize(npt);
+	cmd = "get_current_peaking_times";
+	runWithCheck(xiaBoardOperation, 0, cmd.data(), peakingtimes.data());
+
+	for(int i = 0; i < npt; i++)
 	{
-		std::string status;
-		runWithCheck(getDeviceInfoByIndex, i, 12, serial, &devinterface);
-		status = "Device #" + std::to_string(i) + " S/N " + serial + " ";
-
-		switch(devinterface)
-		{
-			case INTERFACE_ETHERNET_TCP:
-			{
-				runWithCheck(getIPAddress, serial, 12, ipaddr);
-				status += "connected over TCP via IP: ";
-				status += ipaddr;
-				break;
-			}
-			case INTERFACE_ETHERNET_UDP:
-			{
-				status += "connected over UDP.";
-				break;
-			}
-			case INTERFACE_USB:
-			{
-				status += "connected over USB (FT4222).";
-				break;
-			}
-			case INTERFACE_USB_HID:
-			{
-				status += "connected over USB (HID).";
-				break;
-			}
-			case INTERFACE_UNKNOWN:
-			{
-				status += "connected over an unknown interface.";
-				break;
-			}
-		}
-
-		log.info(status.c_str());
+		log.info("Peaking time %d -> %lf", i, peakingtimes[i]);
 	}
 
-	rdb["/user/axas/index"].create(mulex::RdbValueType::INT32, std::int32_t(0));
-	devindex = rdb["/user/axas/index"];
+	// Check length
+	unsigned long mcalen;
+	cmd = "mca_length";
+	runWithCheck(xiaGetRunData, 0, cmd.data(), &mcalen);
 
-	log.info("Using device index %d from '/user/axas/index'.", devindex);
-
-	if(devindex >= std::int32_t(ndevices))
+	if(mcalen == 0)
 	{
-		log.error("Invalid device index. Aborting...");
+		log.error("mca_length returned 0.");
 		return;
 	}
 
-	runWithCheck(getDeviceInfoByIndex, devindex, 12, serial, &devinterface);
+	double nchan = rdb["/user/axas/number_mca_channels"];
+	if(mcalen != static_cast<unsigned long>(nchan))
+	{
+		log.error("mca_length returned unexpected value.");
+		return;
+	}
 
-	setupBinsAndTimings();
+	log.info("Allocating %lu words for MCA...", mcalen * sizeof(unsigned long));
+	mcadata.resize(mcalen * sizeof(unsigned long));
 
 	registerRunStartStop(&AxasMCA::startMeasurement, &AxasMCA::stopMeasurement);
+	registerEvent("axas::mcadata");
 }
 
 AxasMCA::~AxasMCA()
 {
 	// Virtually stop the run
 	stopMeasurement(0);
+
+	xiaExit();
+	xiaCloseLog();
 }
 
-void AxasMCA::setupBinsAndTimings()
+unsigned int AxasMCA::findChannels()
 {
-	// Valid range 25 - 12600 ns
-	rdb["/user/axas/timings/peaking"].create(mulex::RdbValueType::FLOAT32, 1000.0f);
-	FloatType peaking = rdb["/user/axas/timings/peaking"];
-	runWithCheck(setSlowFilterPeakingTime, serial, &peaking);
-	log.info("Setting peaking time to %f ns.", peaking);
+	char module[MAXALIAS_LEN];
+	int nchannels_module = 0;
 
-	// We want realtime soft mode, so no stop condition on de DPP
-	StopConditionType stopcond = NONE;
-	DoubleType mt = 0.0; // NOTE: (César) Should not matter
-	runWithCheck(setStopCondition, serial, stopcond, &mt);
-	log.info("Setting stop contidion to none.");
+	unsigned int nmodules = 0;
+	unsigned int nchannels = 0;
 
-	// No. of bins
-	rdb["/user/axas/bins/size"].create(mulex::RdbValueType::UINT16, std::uint16_t(4096));
-	bins = rdb["/user/axas/bins/size"];
-	rdb["/user/axas/bins/bytes_per_bin"].create(mulex::RdbValueType::UINT8, std::uint8_t(3)); // 24-bit UI inside the DPP
-	bpb = rdb["/user/axas/bins/bytes_per_bin"];
-	runWithCheck(setMCANumberOfBins, serial, &bins);
-	runWithCheck(setMCABytesPerBin, serial, &bpb);
-	log.info("Setting DPP with %d bins (%d bytes per bin).", bins, bpb);
+	std::string command = "number_of_channels";
 
-	mcadata.resize(bins * bpb);
-	status = true;
+	runWithCheck(xiaGetNumModules, &nmodules);
+	log.info("Found #%d XIA modules.", nmodules);
 
-	rdb["/user/axas/stats/realtime"].create(mulex::RdbValueType::FLOAT64, 0.0);
-	rdb["/user/axas/stats/livetime"].create(mulex::RdbValueType::FLOAT64, 0.0);
-	rdb["/user/axas/stats/outrate"].create(mulex::RdbValueType::UINT32, std::uint32_t(0));
-	rdb["/user/axas/stats/inrate"].create(mulex::RdbValueType::UINT32, std::uint32_t(0));
-
-	registerEvent("axas::mcadata");
-}
-
-void AxasMCA::updateStats()
-{
-	RunStatisticsType runstats;
-	runWithCheck(getRunStatistics, serial, &runstats);
-
-	if(!runstats.isRunActive)
+	for(unsigned int i = 0; i < nmodules; i++)
 	{
-		// Early out
-		return;
+		runWithCheck(xiaGetModules_VB, i, module);
+		log.info("Module: %s.", module);
+		runWithCheck(xiaGetModuleItem, module, command.data(), &nchannels_module);
+		nchannels += nchannels_module;
+	}
+
+	return nchannels;
+}
+
+void AxasMCA::update()
+{
+	static double statistics[9];
+	static std::string cmd1 = "mca";
+	static std::string cmd2 = "module_statistics_2";
+
+	for(int c = 0; c < devchannels; c++)
+	{
+		runWithCheck(xiaGetRunData, c, cmd1.data(), mcadata.data());
+		runWithCheck(xiaGetRunData, c, cmd2.data(), statistics);
+
+		if(statistics[0] <= 0.0)
+		{
+			// Run will stop
+			return;
+		}
 	}
 
 	// Stats
-	rdb["/user/axas/stats/realtime"] = runstats.realTime;
-	rdb["/user/axas/stats/livetime"] = runstats.liveTime;
-	rdb["/user/axas/stats/outrate"] = runstats.outputCountRate;
-	rdb["/user/axas/stats/inrate"] = runstats.inputCountRate;
+	rdb["/user/axas/stats/realtime"] = statistics[0];
+	rdb["/user/axas/stats/livetime"] = statistics[1];
+	rdb["/user/axas/stats/outrate"] = statistics[6];
+	rdb["/user/axas/stats/inrate"] = statistics[5];
 
-	// MCA Data buffer
-	runWithCheck(getMCADataRaw, serial, bpb * bins, mcadata.data());
 	dispatchEvent("axas::mcadata", mcadata);
 
 	// Update stats and MCA array every second
-	deferExec(&AxasMCA::updateStats, 1000);
+	deferExec(&AxasMCA::update, 1000);
 }
 
 void AxasMCA::startMeasurement(std::uint64_t runno)
 {
-	if(status)
-	{
-		runWithCheck(startRun, serial);
-		deferExec(&AxasMCA::updateStats);
-	}
+	status = true;
+	runWithCheck(xiaStartRun, -1, static_cast<unsigned short>(0));
+	deferExec(&AxasMCA::update);
 }
 
 void AxasMCA::stopMeasurement(std::uint64_t runno)
 {
 	if(status)
 	{
-		runWithCheck(stopRun, serial);
+		status = false;
+		runWithCheck(xiaStopRun, -1);
 	}
 }
 
